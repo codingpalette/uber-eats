bd2b396468ef7ea37974c1d361d11290c58efde9
codingpalette <codingpalette@gmail.com>
2020-11-22 16:51:33 +0900

    기본 db 연결

---
+++ uber-eats-backend/src/restaurants/dtos/create-restaurant.dto.ts
@@ -0,0 +1,8 @@
+import {InputType, OmitType} from "@nestjs/graphql";
+import {Restaurant} from "../entities/restaurants.entity";
+
+
+@InputType()
+export class CreateRestaurantDto extends OmitType(Restaurant, ['id']) {
+
+}
---
+++ uber-eats-backend/src/restaurants/entities/restaurants.entity.ts
@@ -0,0 +1,41 @@
+import {Field, InputType, ObjectType} from "@nestjs/graphql";
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {IsBoolean, IsOptional, IsString, Length} from "class-validator";
+
+
+@InputType({ isAbstract: true })
+@ObjectType()
+@Entity()
+export class Restaurant {
+
+	@PrimaryGeneratedColumn()
+	@Field(() => Number)
+	id: number;
+
+	@Field(() => String)
+	@Column()
+	@IsString()
+	@Length(5)
+	name: string;
+
+	@Field(() => Boolean, {nullable: true})
+	@Column({default: true})
+	@IsOptional()
+	@IsBoolean()
+	isVegan: boolean;
+
+	@Field(() => String, {defaultValue: '주소 기본값'})
+	@Column()
+	@IsString()
+	address: string;
+
+	@Field(() => String)
+	@Column()
+	@IsString()
+	ownerName: string;
+
+	@Field(() => String)
+	@Column()
+	@IsString()
+	categoryName: string;
+}
